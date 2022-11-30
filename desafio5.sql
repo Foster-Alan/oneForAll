cd29a0ecd8bec97db9376130369da051b898f161
Alan Foster <alan-af@outlook.com>
2022-11-30 17:49:30 -0300

    fix 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT a.cancao AS cancao, COUNT(b.id_cancao) AS reproducoes FROM SpotifyClone.cancoes AS a
 INNER JOIN SpotifyClone.historico_de_reproducoes AS b
-ON a.id_cancao = .id_cancao
+ON a.id_cancao = b.id_cancao
 GROUP BY cancao
-ORDER BY reproducoes DESC, cancao ASC
+ORDER BY reproducoes DESC, cancao
 LIMIT 2;
