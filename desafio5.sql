a6c49521d21a3ba03ed4c9e066fdc5682530151f
Alan Foster <alan-af@outlook.com>
2022-11-30 17:41:30 -0300

    5

---
+++ desafio5.sql
@@ -0,0 +1,6 @@
+SELECT a.cancao AS cancao, COUNT(b.id_cancao) AS reproducoes FROM SpotifyClone.cancoes AS a
+INNER JOIN SpotifyClone.historico_de_reproducoes AS b
+ON a.id_cancao = .id_cancao
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao ASC
+LIMIT 2;
